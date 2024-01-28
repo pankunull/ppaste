3553b27ac7deae490edb3474d5d28b6694679671
pankunull <panku_null@proton.me>
2024-01-28 07:36:52 +0100

    Generated the correct hash

--- shagen.sh
+++ shagen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-openssl sha256 src/ppaste.sh | cut -d ' ' -f2 > sign/sha256sum.txt
+#openssl sha256 src/ppaste.sh | cut -d ' ' -f2 > sign/sha256sum.txt
+
+sha256sum src/ppaste.sh > sign/sha256sum.txt
 
 cat sign/sha256sum.txt
