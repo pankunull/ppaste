88e9c4213718b855ced049b498600aba2afe5c3a
panku_null <panku_null@proton.me>
2024-09-17 20:17:33 +0200

    Version 0.5.24

--- shagen.sh
+++ shagen.sh
@@ -2,6 +2,6 @@
 
 #openssl sha256 src/ppaste.sh | cut -d ' ' -f2 > sign/sha256sum.txt
 
-cat src/ppaste.sh | openssl sha256 | cut -d ' ' -f2 > sign/sha256.txt
+openssl sha256 < src/ppaste.sh | cut -d ' ' -f2 > sign/sha256.txt
 
 cat sign/sha256.txt
